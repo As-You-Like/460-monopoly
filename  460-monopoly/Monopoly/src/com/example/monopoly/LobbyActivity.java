4820f2a1fb43e3a02eb3e2569a337802744da6c3
dmitry11374 <dmitry11374@yahoo.com>
2013-03-15 17:59:55 +0000

    Added listeners and static references to all the activities created in the previous commit

---  460-monopoly/Monopoly/src/com/example/monopoly/LobbyActivity.java
+++  460-monopoly/Monopoly/src/com/example/monopoly/LobbyActivity.java
@@ -3,13 +3,41 @@ package com.example.monopoly;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class LobbyActivity extends Activity {
+	
+	public static LobbyActivity activity = null;
+	
+	private Button btnBack = null;
+	private Button btnRefresh = null;
+	private ListView lstPlayers = null;
+	private TextView txtGameName = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_lobby); 
+		
+		//this.btnBack    = (Button) this.findViewById(R.id.btnBack);
+		this.btnRefresh = (Button) this.findViewById(R.id.btnRefresh);
+		this.lstPlayers   = (ListView) this.findViewById(R.id.lstPlayers);
+		this.txtGameName   = (TextView) this.findViewById(R.id.txtGameName);
+		
+		this.btnRefresh.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				
+				
+				
+			}
+			
+		});
 	}
 
 	@Override
