f6e3ac452a82c83822c961c2197ab0edce4a6ad7
dawonic@gmail.com <dawonic@gmail.com@3193b241-82db-2f6d-4372-caa802cc483e>
2013-03-24 18:58:10 +0000

    This one is my part making two android Activities.  The Activities are “NewLoadActivity” and “SaveGameActivity”. The first Activity is “NewLoadActivity”. this Activity is going to show when click on Host button in Welcome Activity. One of member will make the “WelcomeAvtivity”.

--- trunk/ 460-monopoly/Monopoly/src/com/example/monopoly/NewLoadActivity.java
+++ trunk/ 460-monopoly/Monopoly/src/com/example/monopoly/NewLoadActivity.java
@@ -1,8 +1,19 @@
 package com.example.monopoly;
 
+import static android.widget.Toast.makeText;
+
+import com.example.monopoly.R;
+import com.example.monopoly.R.id;
+import com.example.monopoly.R.layout;
+import com.example.monopoly.R.menu;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class NewLoadActivity extends Activity {
@@ -10,11 +21,24 @@ public class NewLoadActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_newload); 
-		// Test comment
-		// Test comment2
-		
-		Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+		setContentView(R.layout.activity_newload);
+		Button buttonNewGame = (Button) this.findViewById(R.id.btnNewGame);
+		buttonNewGame.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				makeText(getApplicationContext(), "Go to CreateHostActivity", Toast.LENGTH_LONG).show();
+
+			}
+		});
+
+		Button buttonLoadGame = (Button) this.findViewById(R.id.btnLoadGame);
+		buttonLoadGame.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				String actionName = "android.intent.action.SaveGameActivity";
+				Intent intent = new Intent(actionName);
+				startActivity(intent);
+
+			}
+		});
 	}
 
 	@Override
@@ -22,6 +46,6 @@ public class NewLoadActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	} 
+	}
 
 }
---
+++ trunk/ 460-monopoly/Monopoly/src/com/example/monopoly/SaveGameActivity.java
@@ -0,0 +1,56 @@
+package com.example.monopoly;
+
+import static android.widget.Toast.makeText;
+
+import java.util.ArrayList;
+
+import com.example.monopoly.R;
+import com.example.monopoly.R.id;
+import com.example.monopoly.R.layout;
+import com.example.monopoly.R.menu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class SaveGameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_savegame);
+		final ListView listView = (ListView) findViewById(R.id.lstHosts);
+		ArrayList<String> listItems = new ArrayList<String>();
+		for (int i = 0; i < 5; i++) {
+			listItems.add("");
+		}
+
+		final ArrayAdapter<String> myadapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, listItems);
+
+		// Assign adapter to ListView
+		listView.setAdapter(myadapter);
+
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				makeText(getApplicationContext(), "This feature is not available.", Toast.LENGTH_LONG)
+						.show();
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
