5a4ed8a0c46587478096a344d5c364cb9141c07e
rjhebert20 <rjhebert20@gmail.com>
2013-03-24 05:21:38 +0000

    Added: SplashActivity Updated: SplashActivity, SplashActivity XML, WelcomeActivity

---
+++  460-monopoly/Monopoly/src/com/example/monopoly/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.monopoly;
+
+import com.example.bluetooth.Bluetooth;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+	
+	public static SplashActivity activity;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		SplashActivity.activity = this;
+		setContentView(R.layout.activity_splash);
+		new Bluetooth(this.getApplicationContext());
+		Intent intent = new Intent(SplashActivity.activity, WelcomeActivity.class);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_splash, menu);
+		return true;
+	}
+
+}
